570dc083b70263d8391c469e29976d5f4e35a667
Cameron Spencer <cameron@lab43.com>
2020-08-25 19:40:06 -0700

    remove console log

--- index.js
+++ index.js
@@ -2,27 +2,22 @@ const path = require('path');
 
 
 
+// TODO: throw error if name or directory are undefined
+// TODO: resolve subdirectories, e.g. about/team -> children.about.children.team.children
+// TODO: also make sure the slug has the full path
+// TODO: throw error if directory doesn't exist
+
 module.exports = ({ name, directory }) => ({ registerTreeHandler }) => {
 
   registerTreeHandler(({ tree, globals }) => {
-
-    // TODO: throw error if name or directory are undefined
-    // TODO: resolve subdirectories, e.g. about/team -> children.about.children.team.children
-    // TODO: also make sure the slug has the full path
-    // TODO: throw error if directory doesn't exist
     const pages = tree.children[directory].children;
-
     globals[name] = Object.keys(pages).map((slug) => {
       return {
         ...pages[slug],
         slug: path.join(directory, slug),
       };
     });
-
-    console.log(globals[name]);
-
     return {tree, globals};
-
   });
 
 }
